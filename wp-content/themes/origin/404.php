35cefb92a2c28aaacf07087c32cea73106751b61
nghiempvn <nghiempvn@gmail.com>
2015-02-25 09:55:27 +0700

    404

--- wp-content/themes/origin/404.php
+++ wp-content/themes/origin/404.php
@@ -1,7 +1,7 @@
 <?php get_header(); ?>
 
-	<h2>Error 404 - Page Not Found</h2>
-
-<?php get_sidebar(); ?>
+<div class="body" style="min-height: 200px;text-align: left;font-size: 30px;">
+    Page Not found
+</div>
 
 <?php get_footer(); ?>
--- wp-content/themes/origin/footer.php
+++ wp-content/themes/origin/footer.php
@@ -1,6 +1,6 @@
 <div id="footer">    
     <div class="menu-footer group">
-        <?php wp_nav_menu(array('menu' => 'menu-bottom','theme_location' => 'secondary', 'container' => '',)); ?>
+        <?php wp_nav_menu(array('menu' => 'menu-bottom','theme_location' => 'secondary', 'container' => '','menu_class' => 'bottom-menu')); ?>
     </div>
     <div class="footer-infor">
         &copy;© 2013 Soho Projects. All rights resevered. Designed by CYASOFT
