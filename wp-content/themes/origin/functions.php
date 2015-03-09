634504c15efb36447970166fe1f1d51872caec6e
nghiempvn <nghiempvn@gmail.com>
2015-03-09 15:46:38 +0700

    news category

--- wp-content/themes/origin/functions.php
+++ wp-content/themes/origin/functions.php
@@ -1,5 +1,4 @@
 <?php
-
 // Add RSS links to <head> section
 automatic_feed_links();
 
@@ -168,53 +167,68 @@ add_theme_support('custom-header');
 // add class active to menu
 add_filter('nav_menu_css_class', 'special_nav_class', 10, 2);
 
-if ( ! function_exists( 'pm_entry_meta' ) ) :
-/**
- * Set up post entry meta.
- *
- * Prints HTML with meta information for current post: categories, tags, permalink, author, and date.
- *
- * Create your own pm_entry_meta() to override in a child theme.
- *
- * @since Twenty Twelve 1.0
- */
-function pm_entry_meta() {
-	// Translators: used between list items, there is a space after the comma.
-	$categories_list = get_the_category_list( __( ', ', 'pm' ) );
+if (!function_exists('pm_entry_meta')) :
 
-	// Translators: used between list items, there is a space after the comma.
-	$tag_list = get_the_tag_list( '', __( ', ', 'pm' ) );
+    /**
+     * Set up post entry meta.
+     *
+     * Prints HTML with meta information for current post: categories, tags, permalink, author, and date.
+     *
+     * Create your own pm_entry_meta() to override in a child theme.
+     *
+     * @since Twenty Twelve 1.0
+     */
+    function pm_entry_meta() {
+        // Translators: used between list items, there is a space after the comma.
+        $categories_list = get_the_category_list(__(', ', 'pm'));
 
-	$date = sprintf( '<span class="date"><a href="%1$s" title="%2$s" rel="bookmark"><time class="entry-date" datetime="%3$s">%4$s</time></a></span>',
-		esc_url( get_permalink() ),
-		esc_attr( get_the_time() ),
-		esc_attr( get_the_date( 'c' ) ),
-		esc_html( get_the_date() )
-	);
+        // Translators: used between list items, there is a space after the comma.
+        $tag_list = get_the_tag_list('', __(', ', 'pm'));
 
-	$author = sprintf( '<span class="author vcard"><i class="fa fa-user"></i>&nbsp<a class="url fn n" href="%1$s" title="%2$s" rel="author">%3$s</a></span>',
-		esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ),
-		esc_attr( sprintf( __( 'View all posts by %s', 'pm' ), get_the_author() ) ),
-		get_the_author()
-	);
+        $date = sprintf('<span class="date"><a href="%1$s" title="%2$s" rel="bookmark"><time class="entry-date" datetime="%3$s">%4$s</time></a></span>', esc_url(get_permalink()), esc_attr(get_the_time()), esc_attr(get_the_date('c')), esc_html(get_the_date())
+        );
 
-	// Translators: 1 is category, 2 is tag, 3 is the date and 4 is the author's name.
-	if ( $tag_list ) {
-		$utility_text = __( '<div class="entry-meta"><div class="entry-meta-left">%4$s %3$s</div><div class="entry-meta-right">%1$s</div></div>', 'pm' );
-	} elseif ( $categories_list ) {
-		$utility_text = __( '<div class="entry-meta"><div class="entry-meta-left">%4$s %3$s</div><div class="entry-meta-right"><i class="fa fa-folder-open"></i>&nbsp%1$s</div></div>', 'pm' );
-	} else {
-		$utility_text = __( '<div class="entry-meta"><div class="entry-meta-left">%4$s %3$s</div></div>', 'pm' );
-	}
+        $author = sprintf('<span class="author vcard"><i class="fa fa-user"></i>&nbsp<a class="url fn n" href="%1$s" title="%2$s" rel="author">%3$s</a></span>', esc_url(get_author_posts_url(get_the_author_meta('ID'))), esc_attr(sprintf(__('View all posts by %s', 'pm'), get_the_author())), get_the_author()
+        );
 
-	printf(
-		$utility_text,
-		$categories_list,
-		$tag_list,
-		$date,
-		$author
-	);
-}
+        // Translators: 1 is category, 2 is tag, 3 is the date and 4 is the author's name.
+        if ($tag_list) {
+            $utility_text = __('<div class="entry-meta"><div class="entry-meta-left">%4$s %3$s</div><div class="entry-meta-right">%1$s</div></div>', 'pm');
+        } elseif ($categories_list) {
+            $utility_text = __('<div class="entry-meta"><div class="entry-meta-left">%4$s %3$s</div><div class="entry-meta-right"><i class="fa fa-folder-open"></i>&nbsp%1$s</div></div>', 'pm');
+        } else {
+            $utility_text = __('<div class="entry-meta"><div class="entry-meta-left">%4$s %3$s</div></div>', 'pm');
+        }
+
+        printf(
+                $utility_text, $categories_list, $tag_list, $date, $author
+        );
+    }
+
+endif;
+if (!function_exists('pm_content_nav')) :
+
+    /**
+     * Displays navigation to next/previous pages when applicable.
+     *
+     * @since Twenty Twelve 1.0
+     */
+    function pm_content_nav($html_id) {
+        global $wp_query;
+
+        $html_id = esc_attr($html_id);
+
+        if ($wp_query->max_num_pages > 1) :
+            ?>
+            <nav id="<?php echo $html_id; ?>" class="navigation" role="navigation">
+                <h3 class="assistive-text"><?php _e('Post navigation', 'pm'); ?></h3>
+                <div class="nav-previous"><?php next_posts_link(__('<span class="meta-nav">&larr;</span> Older posts', 'pm')); ?></div>
+                <div class="nav-next"><?php previous_posts_link(__('Newer posts <span class="meta-nav">&rarr;</span>', 'pm')); ?></div>
+            </nav><!-- #<?php echo $html_id; ?> .navigation -->
+        <?php
+        endif;
+    }
+
 endif;
 
 function special_nav_class($classes, $item) {
--- wp-content/themes/origin/sidebar.php
+++ wp-content/themes/origin/sidebar.php
@@ -1,39 +1,17 @@
-<aside>
+<?php
+/**
+ * The sidebar containing the main widget area
+ *
+ * If no active widgets are in the sidebar, hide it completely.
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twelve
+ * @since Twenty Twelve 1.0
+ */
+?>
 
-    <?php if (function_exists('dynamic_sidebar') && dynamic_sidebar('Sidebar Widgets')) : else : ?>
-    
-        <!-- All this stuff in here only shows up if you DON'T have any widgets active in this zone -->
-
-    	<?php get_search_form(); ?>
-    
-    	<?php wp_list_pages('title_li=<h2>Pages</h2>' ); ?>
-    
-    	<h2>Archives</h2>
-    	<ul>
-    		<?php wp_get_archives('type=monthly'); ?>
-    	</ul>
-        
-        <h2>Categories</h2>
-        <ul>
-    	   <?php wp_list_categories('show_count=1&title_li='); ?>
-        </ul>
-        
-    	<?php wp_list_bookmarks(); ?>
-    
-    	<h2>Meta</h2>
-    	<ul>
-    		<?php wp_register(); ?>
-    		<li><?php wp_loginout(); ?></li>
-    		<li><a href="http://wordpress.org/" title="Powered by WordPress, state-of-the-art semantic personal publishing platform.">WordPress</a></li>
-    		<?php wp_meta(); ?>
-    	</ul>
-    	
-    	<h2>Subscribe</h2>
-    	<ul>
-    		<li><a href="<?php bloginfo('rss2_url'); ?>">Entries (RSS)</a></li>
-    		<li><a href="<?php bloginfo('comments_rss2_url'); ?>">Comments (RSS)</a></li>
-    	</ul>
-	
+	<?php if ( is_active_sidebar( 'sidebar-news' ) ) : ?>
+		<div id="secondary" class="widget-area" role="complementary">
+			<?php dynamic_sidebar( 'sidebar-news' ); ?>
+		</div><!-- #secondary -->
 	<?php endif; ?>
-
-</aside>
